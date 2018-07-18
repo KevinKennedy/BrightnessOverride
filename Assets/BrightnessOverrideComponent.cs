18f500e4b6f6b014e25f6cfbe0511000fa1b32ed
Kevin K <6423774+KevinKennedy@users.noreply.github.com>
2018-07-18 09:44:57 -0700

    Initial Commit

---
+++ Assets/BrightnessOverrideComponent.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if NETFX_CORE
+using Windows.Graphics.Display;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+#endif
+
+public class BrightnessOverrideComponent : MonoBehaviour
+{
+#if NETFX_CORE
+    private BrightnessOverride bo;
+    private void InitBo()
+    {
+        UnityEngine.WSA.Application.InvokeOnUIThread(() =>
+        {
+            this.bo = BrightnessOverride.GetForCurrentView();
+        }, true);
+    }
+
+    public float Brightness
+    {
+        get
+        {
+            if(this.bo != null) return (float)this.bo.BrightnessLevel;
+            return 0f;
+        }
+
+        set
+        {
+            if(this.bo != null)
+            {
+                double newBrightness = (double)value;
+
+                if (newBrightness > 1.0)
+                {
+                    newBrightness = 1.0;
+                }
+                if (newBrightness < 0.0)
+                {
+                    newBrightness = 0.0;
+                }
+
+                bo.SetBrightnessLevel(newBrightness, DisplayBrightnessOverrideOptions.None);
+            
+                bo.StartOverride();
+            }
+        }
+    }
+
+    private void DisableBo()
+    {
+        if(this.bo != null)
+        {
+            this.bo.StopOverride();
+        }
+    }
+
+#else
+    private void InitBo() { }
+    public float Brightness { get; set; }
+    private void DisableBo() { }
+#endif
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        this.InitBo();
+        this.Brightness = 1.0f;
+    }
+
+    public void OnDisable()
+    {
+        this.DisableBo();
+    }
+
+}
---
+++ Assets/KeywordManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class KeywordManager : MonoBehaviour
+{
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    public BrightnessOverrideComponent BrightnessOverride;
+    public GameObject Content;
+
+    // Use this for initialization
+    void Start()
+    {
+        keywords.Add("Off", () =>
+        {
+            this.BrightnessOverride.Brightness = 0f;
+        });
+
+        keywords.Add("On", () =>
+        {
+            this.BrightnessOverride.Brightness = 1.0f;
+        });
+
+        keywords.Add("Darker", () =>
+        {
+            this.BrightnessOverride.Brightness -= 0.1f;
+        });
+        
+        keywords.Add("Brighter", () =>
+        {
+            this.BrightnessOverride.Brightness += 0.1f;
+        });
+
+        keywords.Add("Hide", () =>
+        {
+            this.Content.SetActive(false);
+        });
+
+        keywords.Add("Show", () =>
+        {
+            this.Content.SetActive(true);
+        });
+
+        keywords.Add("Quit", () =>
+        {
+            Application.Quit();
+        });
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and start recognizing!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+}
